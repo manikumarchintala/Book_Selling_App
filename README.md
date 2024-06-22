26fc3d60ead9c46e3970267fb734c2387fbcd1ce
Mani Kumar <manikumarchintala@gmail.com>
2024-06-22 06:53:08 -0500

    structure

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # booksellingapp
 
-a small web app
+a small book store web app the idea is to make a webapp that unables the user to add books to the cart and either they can checkout the items in the cart or else they can remove it from the cart .
+
+as for the client side the idea is to use html and css and vanilla js .
+for the serever side node and express
+and for the database its going to be mongo db.
--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const zod = require("zod");
 app.use(bodyParser.json());
 const myschema = zod.string();
 const jwt = require("jsonwebtoken");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 //connceting to db;
 const secret = process.env.secret;
 const mongouri = process.env.Mongodburl;
-mongoose.connect(``);
+mongoose.connect(`mongodb+srv://${mongouri}@cluster0.9769s5d.mongodb.net/`);
 //defineing schema for admin
 const adminschema = new mongoose.Schema({
   username: String,
@@ -25,7 +25,7 @@ const userschema = new mongoose.Schema({
   auth: String,
   addedBooks: [
     {
-      //
+      //each type schema is associated with a objectid in db
       type: mongoose.Schema.Types.ObjectId,
       ref: "Book",
     },
@@ -33,7 +33,9 @@ const userschema = new mongoose.Schema({
 });
 //definening schema for Book
 const bookschema = new mongoose.Schema({
+  title: String,
   genre: String,
+  author: String,
   descreption: String,
   imagelink: String,
   price: Number,
@@ -42,14 +44,12 @@ const bookschema = new mongoose.Schema({
 const Admin = mongoose.model("Admin", adminschema);
 const User = mongoose.model("User", userschema);
 const Books = mongoose.model("Books", bookschema);
-
 //defining admin middleware for auth
-
 function Adminmiddleware(req, res, next) {
   const username = myschema.parse(req.headers.username);
-  const password = myschema.min(15).parse(req.headers.password);
+  const password = myschema.min(8).parse(req.headers.password);
   const auth = req.headers.authorization;
-  User.findOne({ auth: auth }).then(function (value) {
+  Admin.findOne({ auth: auth }).then(function (value) {
     if (value) {
       next();
     } else {
@@ -57,31 +57,28 @@ function Adminmiddleware(req, res, next) {
     }
   });
 }
-
 //defining user middleware for auth
-
 function usermiddlware(req, res, next) {
   const username = myschema.parse(req.headers.username);
   const password = myschema.min(8).parse(req.headers.password);
   const auth = req.headers.authorization;
-  User.findOne({ auth: auth }).then(function (value) {
+  User.find({ password: password, username: username }).then(function (value) {
     if (value) {
       next();
     } else {
-      res.status(403).json({ msg: "inavlid admin" });
+      res.status(403).json({ msg: "user" });
     }
   });
 }
-
-//posting
-app.post("admin/signup", async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.username;
+//posting to sign up for the email
+app.post("/admin/signup", async (req, res) => {
+  console.log("hooo");
+  let username = req.headers.username;
+  let password = req.headers.password;
   let auth = jwt.sign(
     {
       username: username,
       password: password,
-      auth: auth,
     },
     secret
   );
@@ -92,6 +89,101 @@ app.post("admin/signup", async (req, res) => {
   });
   res.json({
     message: "admin created sucessufully",
+  });
+});
+//
+app.post("/admin/signin", async (req, res) => {
+  const username = myschema.parse(req.headers.username);
+  const password = myschema.min(8).parse(req.headers.password);
+  let newbook = await Admin.findOne({
+    username: username,
+  });
+  res.json({
+    auth: newbook.auth,
+  });
+});
+//
+app.get("/admin/view", Adminmiddleware, async (req, res) => {
+  console.log("authworked");
+  let response = await Books.find({});
+  res.json({
+    Books: response,
+  });
+});
+//posting a
+app.post("/admin/addbook", Adminmiddleware, async (req, res) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const genre = req.body.genre;
+  const descreption = req.body.descreption;
+  const imagelink = req.body.imagelink;
+  const price = req.body.price;
+  let newbook = await Books.create({
+    title: title,
+    author: author,
+    genre: genre,
+    descreption: descreption,
+    imagelink: imagelink,
+    price: price,
+  });
+  res.json({
+    message: "course created sucessfully",
+    BookId: newbook._id,
+  });
+});
+//creating user base
+app.post("/user/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username: username,
+    password: password,
+  });
+  res.json({
+    msg: "user created",
+  });
+});
+app.get("/users/view", usermiddlware, async (req, res) => {
+  let response = await Books.find({});
+  res.json({
+    Books: response,
+  });
+});
+//adding to cart
+app.post("/books/:bookId", usermiddlware, async (req, res) => {
+  // Implement adding book logic
+  const bookId = req.params.bookId;
+  const username = req.headers.username;
+  console.log("here");
+  await User.updateOne(
+    {
+      username: username,
+    },
+    {
+      addedBooks: {
+        $push: {
+          addedBooks: bookId,
+        },
+      },
+    }
+  );
+  res.json({
+    msg: "purchase complete",
+  });
+});
+//retriving data from the cart
+app.get("/purchasedCourses", usermiddlware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+  const book = await Books.find({
+    _id: {
+      $in: User.bookId,
+    },
+  });
+  res.json({
+    book: Books,
   });
 });
 //opening a port for the server
